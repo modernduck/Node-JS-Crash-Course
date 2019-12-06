4fb251570c6838ac9711ece397420485d5003ca6
modernduck <sompop.kulapalanont@gmail.com>
2019-12-06 22:01:26 +0700

    first commit

---
+++ helloworld.js
@@ -0,0 +1,3 @@
+//this is how we use comment
+console.log("Hello World") //no need to put semi colon and code still work this is javascript
+console.log("A", (3 + 5 )); // another way to use console.log
