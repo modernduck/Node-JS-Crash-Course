25904ba84bb096ba24956279bd38a7600cf9b4d7
modernduck <sompop.kulapalanont@gmail.com>
2019-12-07 14:24:28 +0700

    update report with code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 var inputFileName = process.argv[2];
+var outputFileName = process.argv[3];
 console.log("Reading file from ", inputFileName)
 var rawJson = fs.readFileSync(inputFileName)
 var data = JSON.parse(rawJson);
@@ -11,4 +12,13 @@ var report = {
     avg: (sum/ages.length),
     count: ages.length
 }
-console.log(report);
+//console.log(report);
+//going to write file
+fs.writeFile(outputFileName, JSON.stringify(report), err =>{
+    //will run when error  or finish
+    if(err){
+        throw error;
+    }else{
+        console.log('Done write file');
+    }
+});
